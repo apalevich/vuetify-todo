9d065d6929ab6feac796a3c4dbe525ebf0bb8c7d
Artyom Palevich <hello@apalevich.ru>
2022-08-24 14:08:39 +0300

    Done and Delete task implemented

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -8,7 +8,10 @@
           v-for="task in tasks"
           :key="task.id"
       >
-        <v-list-item @click="doneTask(task.id)">
+        <v-list-item
+          @click="doneTask(task.id)"
+          :class="{ 'blue lighten-5': task.done }"
+        >
           <template>
             <v-list-item-action>
               <v-checkbox
@@ -18,8 +21,22 @@
             </v-list-item-action>
 
             <v-list-item-content>
-              <v-list-item-title>{{ task.title }}</v-list-item-title>
+              <v-list-item-title
+                :class="{ 'text-decoration-line-through': task.done }"
+              >
+                {{ task.title }}
+              </v-list-item-title>
             </v-list-item-content>
+
+            <v-list-item-action>
+              <v-btn
+                icon
+                @click.stop="deleteTask(task.id)"
+              >
+                <v-icon color="secondary lighten-1">mdi-delete</v-icon>
+              </v-btn>
+            </v-list-item-action>
+
           </template>
         </v-list-item>
         <v-divider></v-divider>
@@ -56,6 +73,9 @@
       doneTask(id) {
         const task = this.tasks.filter(task => task.id === id)[0]; // better support than .find()
         task.done = !task.done;
+      },
+      deleteTask(id) {
+        this.tasks = this.tasks.filter(task => task.id !== id);
       }
     }
   }
