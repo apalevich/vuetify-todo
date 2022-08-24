ecbd8ff14d7851cfbafa892a4dc45ad0b15324c5
Artyom Palevich <hello@apalevich.ru>
2022-08-24 12:17:04 +0300

    Delete dummy component

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,15 +1,11 @@
 <template>
-  <hello-world />
+<div class="home pa-6">
+  <h1>Todo page</h1>
+</div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
   export default {
     name: 'Todo',
-
-    components: {
-      HelloWorld,
-    },
   }
 </script>
